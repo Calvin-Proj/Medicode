9876b96746eda2d6b788a5e894876b84f0e096e7
Calvin-Proj <s218102909@mandela.ac.za>
2020-12-30 22:54:13 +0200

    cleaned code

--- resources/views/components/stud-sick-ico.blade.php
+++ resources/views/components/stud-sick-ico.blade.php
@@ -1,2 +1,11 @@
-<svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M16.7 5.3a1 1 0 010 1.4l-8 8a1 1 0 01-1.4 0l-4-4a1 1 0 011.4-1.4L8 12.58l7.3-7.3a1 1 0 011.4 0z" clip-rule="evenodd"/>
-</svg>
+<form action="{{ route('studentbooksicktest') }}" method="get" >
+    <button type="submit" class="inline-flex h-full py-4 px-3 hover:bg-white hover:text-primary focus:outline-none focus:text-highlight">
+        <div class="flex items-center">
+            <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M16.7 5.3a1 1 0 010 1.4l-8 8a1 1 0 01-1.4 0l-4-4a1 1 0 011.4-1.4L8 12.58l7.3-7.3a1 1 0 011.4 0z" clip-rule="evenodd"/>
+            </svg>
+            <span class="w-2">
+            </span>
+            Book Sick Test
+        </div>
+    </button>
+</form>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,16 +36,7 @@
                                     @break
                                 @case('student')
                                             <x-test-sched-ico/>
-                                <form action="{{ route('studentbooksicktest') }}" method="get" >
-                                    <button type="submit" class="inline-flex h-full py-4 px-3 hover:bg-white hover:text-primary focus:outline-none focus:text-highlight">
-                                        <div class="flex items-center">
                                             <x-stud-sick-ico/>
-                                            <span class="w-2">
-                                            </span>
-                                            Book Sick Test
-                                        </div>
-                                    </button>
-                                </form>
                                     @break
                                 @default
                             @endswitch
