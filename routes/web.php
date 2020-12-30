65c6aaaf88e49d521be30cef8e8221394a7bafcb
Calvin-Proj <s218102909@mandela.ac.za>
2020-12-30 14:11:43 +0200

    added more authentication
    created usertype middleware for authentication

--- routes/web.php
+++ routes/web.php
@@ -16,26 +16,34 @@ use App\Http\Controllers\StudentController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//Main route for Dashboard
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-//admin routes
+//admin routes + Multi authenticate
+Route::middleware(['checkUsertype:admin'])->group(function(){
 Route::get('/admin/managemodule', [AdminController::class, 'indexmodule'])->name('adminmanagemodules');
 Route::get('/admin/managevenue', [AdminController::class, 'indexvenue'])->name('adminmanagevenues');
 Route::get('/admin/managelecturer', [AdminController::class, 'indexlecturer'])->name('adminmanagelecturers');
 Route::get('/admin/manageinvig', [AdminController::class, 'indexinvig'])->name('adminmanageinvigs');
 Route::get('/admin/managestudent', [AdminController::class, 'indexstudent'])->name('adminmanagestudents');
-//lecturer routes
+});
+//lecturer routes + Multi authenticate
+Route::middleware(['checkUsertype:lect'])->group(function(){
 Route::get('/lecturer/managetests', [LecturerController::class, 'indextest'])->name('lecturermanagetest');
 Route::get('/lecturer/managesicknotes', [LecturerController::class, 'indexsicknotes'])->name('lecturermanagesicktest');
 Route::get('/lecturer/manageattendants', [LecturerController::class, 'indexattend'])->name('lecturermanageattend');
 Route::get('/lecturer/managemisconduct', [LecturerController::class, 'indexmiscon'])->name('lecturermanagemiscon');
-//invig routes
+});
+//invig routes + Multi authenticate
+Route::middleware(['checkUsertype:invig'])->group(function(){
 Route::get('/invig/schedule', [InvigController::class, 'indexinvigschedule'])->name('invigschedules');
 Route::get('/invig/submisconduct', [InvigController::class, 'indexinvigmiscon'])->name('invigmisconduct');
 Route::get('/invig/subhours', [InvigController::class, 'indexinvighours'])->name('invighours');
-//student routes
+});
+//student routes + Multi authenticate
+Route::middleware(['checkUsertype:stud'])->group(function(){
 Route::get('/student/testschedule', [StudentController::class, 'indextestsched'])->name('studenttestsched');
 Route::get('/student/booksicktest', [StudentController::class, 'indexbooktest'])->name('studentbooksicktest');
+});
 
 require __DIR__.'/auth.php';
