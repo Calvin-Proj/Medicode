5ed8e8efd4f575f0d965d88ad1695e77d9e7d178
Calvin-Proj <s218102909@mandela.ac.za>
2021-01-01 18:26:19 +0200

    Revert "Revert "started db design""
    This reverts commit 26ca33c21ac9b25737aeaae0730cbb1291e6ccc3.

---
+++ database/migrations/2021_01_01_143739_create_test_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTestTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('test', function (Blueprint $table) {
+            $table->id('testid');
+            $table->dateTime('test_date');
+            $table->boolean('test_type');
+            $table->text('test_desc');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('test');
+    }
+}
