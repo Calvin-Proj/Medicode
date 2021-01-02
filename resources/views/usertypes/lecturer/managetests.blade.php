7eaecf8ff7681de6dc71a51128a57c642f05e092
Calvin-Proj <s218102909@mandela.ac.za>
2021-01-02 16:13:54 +0200

    datable stylesheet #2

--- resources/views/usertypes/lecturer/managetests.blade.php
+++ resources/views/usertypes/lecturer/managetests.blade.php
@@ -19,6 +19,12 @@
                             <td>3</td>
                             <td>4</td>
                         </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>2</td>
+                            <td>3</td>
+                            <td>4</td>
+                        </tr>
                 </tbody>
         </table>
 </div>
