0511f8d01540d45ad656cad57ff1c02306cb8f45
Calvin-Proj <s218102909@mandela.ac.za>
2021-01-04 00:49:18 +0200

    yajra fix

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -23,22 +23,8 @@ class StudentController extends Controller
     {
         return view('usertypes.student.booksicktest');
     }
-    public function index(Request $request)
+    public function index()
     {
-        if ($request->ajax()) {
-            $data = User::select('*');
-            return DataTables::of($data)
-                    ->addIndexColumn()
-                    ->addColumn('action', function($row){
 
-                           $btn = '<a href="javascript:void(0)" class="edit btn btn-primary btn-sm">View</a>';
-
-                            return $btn;
-                    })
-                    ->rawColumns(['action'])
-                    ->make(true);
-        }
-
-        return view('usertypes.student.testsched');
     }
 }
