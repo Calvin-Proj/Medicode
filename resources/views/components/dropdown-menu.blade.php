f3186bc6c9b042ea30b1915fcad04fe55f01dfcd
Marcelol123 <74250202+Marcelol123@users.noreply.github.com>
2021-01-05 18:05:52 +0200

    removed css

--- resources/views/components/dropdown-menu.blade.php
+++ resources/views/components/dropdown-menu.blade.php
@@ -3,35 +3,22 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
-.dropbtn {
-  padding: 16px;
-}
-
-.dropdown {
-  position: relative;
-}
-
-.dropdown-content {
-  display: none;
-  position: absolute;
-}
 
 .dropdown:hover .dropdown-content {display: block;}
-
 
 </style>
 </head>
 <body>
-<div class="dropdown relative flex-auto justify-center px-4">
+<div class="dropdown flex-auto justify-center px-4">
   <div class="flex items-center">
-    <button class="dropbtn">{{auth()->user()->name}}</button>
+    <button class="dropbtn p-4">{{auth()->user()->name}}</button>
 
     <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
       <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
     </svg>
   </div>
 
-  <div class="dropdown-content flex-auto justify-center rounded-lg p-4 shadow-xl bg-white">
+  <div class="dropdown-content flex-auto justify-center rounded-lg p-4 shadow-xl bg-white hidden absolute">
     <a href="#" class="hover:bg-secondary hover:text-white text-black p-2 py-2 block">Account Settings</a>
     <form action="{{ route('logout') }}" method="post" class="flex min-width-max">
       @csrf
