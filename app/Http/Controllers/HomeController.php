f698ae6b19ae1d55da73192cd49347c5fee3283c
s219802254 <s219802254@mandela.ac.za>
2021-01-31 16:32:34 +0200

    Admin Dashboard Queries- Completed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Models\Module;
+use App\Models\Building;
+use App\Models\Venue;
+use App\Models\Test;
+
 use Illuminate\Http\Request;
 
 
@@ -21,7 +25,47 @@ class HomeController extends Controller
         switch ($data) {
 
             case 'admin':
-                return view('usertypes.admin.homeAdmin') ;
+                $users=User::where('usertype', 'lecturer')->get();
+                $users_lect=count($users);
+               
+
+                $users=User::where('usertype', 'student')->get();
+                $users_stud=count($users);
+
+
+                $users=User::where('usertype', 'invig')->get();
+                $users_invig=count($users);
+
+                $users=User::where('usertype', 'invig')->get();
+                $users_invig=count($users);
+
+                $modules=Module::all();
+                $count_Modules=count($modules);
+
+                $buildings=Building::all();
+                $count_Buildings=count($buildings);
+
+                $venues=Venue::all();
+                $count_Venues=count($venues);
+
+                $tests=Test::where('test_type', 'Standard Test')->get();
+                $count_Tests=count($tests);
+
+                $sick_tests=Test::where('test_type', 'Sick Test')->get();
+                $count_sTests=count($sick_tests);
+
+
+                 $currentDate = date("Y-m-d");
+
+                 $tests_completed = Test::where('test_date','<',$currentDate)
+                 ->where('test_type', 'Standard Test')->get();
+                 $count_CompletedT=count($tests_completed);
+
+                 $sTests_completed = Test::where('test_date','<',$currentDate)
+                 ->where('test_type', 'Sick Test')->get();
+                 $count_CompletedS = count($sTests_completed);
+
+                return view('usertypes.admin.homeAdmin', compact('users_lect','users_stud','users_invig','count_Modules','count_Venues','count_Tests','count_sTests','count_CompletedT','count_CompletedS'));
                 break;
 
 
