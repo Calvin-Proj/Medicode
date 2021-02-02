b8d6be2b3ece799e4eaee5289cb3fb243b0d48b9
Calvin-Proj <s218102909@mandela.ac.za>
2021-02-02 14:25:59 +0200

    1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,35 +30,6 @@
       src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCNNBeMyneLT_Vzd26QqR-A1QYbZAnNT1M&callback=initMap&libraries=&v=weekly"
       defer
     ></script>
-/>
-    <style type="text/css">
-      /* Always set the map height explicitly to define the size of the div
-       * element that contains the map. */
-      #map {
-        height: 100%;
-      }
-
-      /* Optional: Makes the sample page fill the window. */
-      html,
-      body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-      }
-    </style>
-    <script>
-      let map;
-
-      function initMap() {
-        map = new google.maps.Map(document.getElementById("map"), {
-          center: { lat: -34.397, lng: 150.644 },
-          zoom: 8,
-        });
-      }
-    </script>
-
-
-
     <title>NMU Test Schedular</title>
     </head>
     <body class="bg-primary">
