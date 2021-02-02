1286a52b79590ff6aa2ef981f8abc0f02b9dedac
Marcelol123 <74250202+Marcelol123@users.noreply.github.com>
2021-02-02 17:44:56 +0200

    svg

--- resources/views/components/admin-assign-invigilator-btn.blade.php
+++ resources/views/components/admin-assign-invigilator-btn.blade.php
@@ -1,7 +1,9 @@
 <form action="{{ route('adminassigninvigs') }}" method="get" >
     <button type="submit" class="inline-flex h-full py-4 px-3 hover:bg-highlight hover:text-black focus:outline-none focus:text-highlight">
         <div class="flex items-center">
-            <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path d="M10 12a2 2 0 100-4 2 2 0 000 4z"/><path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd"/></svg>
+            <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"/>
+            </svg>
             <span class="w-2">
         </span>
         Assign Invigilator
